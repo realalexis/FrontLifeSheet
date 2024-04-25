2159e616dfbc5194978d010f214a978aaf4423e7
realalexis <alexismoulliere44@gmail.com>
2024-04-25 14:08:36 +0200

    add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,39 @@ import {
   } from "@/components/ui/menubar"
 import { ThemeProvider } from "@/components/theme-provider"
 import { ModeToggle } from "@/components/mode-toggle";
-
+import {Home} from "@/pages/Home.tsx";
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import {Sidebar} from "@/components/Sidebar.tsx";
 const App = () =>{
     return (
         <ThemeProvider>
-            <div>
-                <ModeToggle />
-            {/* <GraphPanel /> */}
-            {/* <Button>Click me</Button> */}
-            <Menubar>
-                <MenubarMenu>
-                    <MenubarTrigger>Year</MenubarTrigger>
-                    <MenubarTrigger>Quarter</MenubarTrigger>
-                    <MenubarTrigger>Month</MenubarTrigger>
-                    <MenubarTrigger>Week</MenubarTrigger>
-                    <MenubarTrigger>Day</MenubarTrigger>
-                </MenubarMenu>
-            </Menubar>
-        </div>
+            <Router>
+                <div>
+                    <ModeToggle />
+                    {/* <GraphPanel /> */}
+                    {/* <Button>Click me</Button> */}
+
+                    <Menubar>
+                        <MenubarMenu>
+                            <MenubarTrigger>Year</MenubarTrigger>
+                            <MenubarTrigger>Quarter</MenubarTrigger>
+                            <MenubarTrigger>Month</MenubarTrigger>
+                            <MenubarTrigger>Week</MenubarTrigger>
+                            <MenubarTrigger>Day</MenubarTrigger>
+                        </MenubarMenu>
+                    </Menubar>
+                    <div className="flex">
+                        <Sidebar />
+
+                        <Routes>
+                            <Route path="/home" element={<Home /> }/>
+                            <Route path="/mood" element={<Home /> }/>
+                            <Route path="/sport" element={<Home /> }/>
+                            <Route path="/sleep" element={<Home />}/>
+                        </Routes>
+                    </div>
+                </div>
+            </Router>
         </ThemeProvider>
     )
 }
