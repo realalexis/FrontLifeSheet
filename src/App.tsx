1253cb3ca03f813109a768671633cd3a5e63a8a5
Amy <yambeddm@gmail.com>
2024-04-25 11:17:35 +0200

    dark_mode added, toggle added, working on time_bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import GraphPanel from "./component/graphique/GraphPanel.tsx";
+// import GraphPanel from "./component/graphique/GraphPanel.tsx";
+// import { Button } from "@/components/ui/button";
+import {
+    Menubar,
+    MenubarMenu,
+    MenubarTrigger,
+  } from "@/components/ui/menubar"
+  import { ThemeProvider } from "@/components/theme-provider"
 
 const App = () =>{
     return (
-        <div>
-            <GraphPanel />
+        <ThemeProvider>
+            <div>
+            {/* <GraphPanel /> */}
+            {/* <Button>Click me</Button> */}
+            <Menubar>
+                <MenubarMenu>
+                    <MenubarTrigger>Year</MenubarTrigger>
+                    <MenubarTrigger>Quarter</MenubarTrigger>
+                    <MenubarTrigger>Month</MenubarTrigger>
+                    <MenubarTrigger>Week</MenubarTrigger>
+                    <MenubarTrigger>Day</MenubarTrigger>
+                </MenubarMenu>
+            </Menubar>
         </div>
+        </ThemeProvider>
     )
 }
 
