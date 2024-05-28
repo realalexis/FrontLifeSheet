5650bae8ec19010d74c41ae58af77641490ff3da
realalexis <alexismoulliere44@gmail.com>
2024-05-28 18:53:56 +0200

    delete barchart

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -7,10 +7,10 @@ import {
   ResponsiveContainer,
   AreaChart,
   Area,
+  BarChart,
+  Bar,
 } from "recharts";
-import { BarGraph } from "./BarChart";
 import { GraphProps, IData } from "@/types/GraphDataTypes";
-
 const monthNames = [
   "January",
   "February",
@@ -28,6 +28,8 @@ const monthNames = [
 
 const Graph = ({ category, selectedTimeUnit, date, type }: GraphProps) => {
   const [data, setData] = useState<IData[]>([]);
+  const [barData, setBarData] = useState([]);
+  const [valueCategory, setValueCategory] = useState("");
 
   const fetchGraphData = useCallback(async () => {
     let url = "";
@@ -43,7 +45,6 @@ const Graph = ({ category, selectedTimeUnit, date, type }: GraphProps) => {
 
     const response = await fetch(url);
     const data = await response.json();
-    console.log("graph", data);
     data.sort(
       (a: { month: number }, b: { month: number }) => a.month - b.month
     );
@@ -51,13 +52,44 @@ const Graph = ({ category, selectedTimeUnit, date, type }: GraphProps) => {
       ...item,
       month: monthNames[item.month - 1],
     }));
+    if (type === "bar") {
+      const count = dataWithMonthNames.reduce(
+        (acc: { name: string; count: number }[], curr: { data: string }) => {
+          const found = acc.find((el) => el.name === curr.data);
+          if (found) {
+            found.count++;
+          } else {
+            acc.push({ name: curr.data, count: 1 });
+          }
+          return acc;
+        },
+        []
+      );
+      console.log(count);
+      setBarData(count);
+      return;
+    }
     console.log(data);
     setData(dataWithMonthNames);
-  }, [category, selectedTimeUnit, date]);
+  }, [category, date, selectedTimeUnit, type]);
 
   useEffect(() => {
     fetchGraphData();
   }, [fetchGraphData]);
+
+  useEffect(() => {
+    switch (category) {
+      case "weight":
+        setValueCategory("kg");
+        break;
+      case "mood":
+        setValueCategory("moodLvl");
+        break;
+
+      default:
+        setValueCategory("");
+    }
+  }, [category]);
 
   return (
     <div className="w-full max-w-2xl h-full border border-slate-700 p-2 m-2 rounded-md bg-slate-100">
@@ -85,9 +117,7 @@ const Graph = ({ category, selectedTimeUnit, date, type }: GraphProps) => {
                 if (active && payload) {
                   return (
                     <div className="bg-white text-black  p-2">
-                      <p>{`${label} : ${payload[0]?.payload?.data} ${
-                        category === "weight" ? "kg" : ""
-                      }`}</p>
+                      <p>{`${label} : ${payload[0]?.payload?.data} ${valueCategory}`}</p>
                       <p>{}</p>
                     </div>
                   );
@@ -104,7 +134,43 @@ const Graph = ({ category, selectedTimeUnit, date, type }: GraphProps) => {
           </AreaChart>
         </ResponsiveContainer>
       ) : (
-        <BarGraph data={data} />
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart
+            width={500}
+            height={300}
+            data={barData}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+            barSize={20}
+          >
+            <XAxis
+              dataKey="name"
+              scale="point"
+              padding={{ left: 10, right: 10 }}
+            />
+            <YAxis />
+            <Tooltip
+              content={({ active, payload, label }) => {
+                if (active && payload) {
+                  return (
+                    <div className="bg-white text-black  p-2">
+                      <p>{`${label} : ${payload[0]?.payload?.count}`}</p>
+                      <p>{}</p>
+                    </div>
+                  );
+                }
+                return null;
+              }}
+            />
+
+            <CartesianGrid strokeDasharray="3 3" />
+            <Bar dataKey="count" fill="#8884d8" background={{ fill: "#eee" }} />
+          </BarChart>
+        </ResponsiveContainer>
       )}
     </div>
   );
